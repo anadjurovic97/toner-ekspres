012945e26fb578fa4a09f76a734c512738cf1db5
Ana Djurovic <anadjurovic129@gmail.com>
2023-03-14 13:09:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// Mobile Menu
+
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar_menu");
+
+function mobileMenu() {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+}
+
+menu.addEventListener("click", mobileMenu);
+
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+menuLinks.addEventListener("click", hideMobileMenu);
+
+// Form
+
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
+
+// Scroll reveal left
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("load", reveal);
